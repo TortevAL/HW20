d316038c6bf95751000cf9112588aae1fbe0abda
TortevAL <165218904+TortevAL@users.noreply.github.com>
2024-05-20 20:22:05 +0300

    Update HW20.py

--- HW20.py
+++ HW20.py
@@ -1,9 +1,9 @@
 class House:
 
     def __init__(self, numberOfFloors):
-        self.numberOfFloors = numberOfFloors
+        self.numberOfFloors = 10
         for i in range(0, 10):
             numberOfFloors += 1
             print("Текущий этаж равен", numberOfFloors)
 
-name = House(10)
+name = House(0)
